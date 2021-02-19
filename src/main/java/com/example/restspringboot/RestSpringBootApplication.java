3144a179e8b0265b21157e6c9ba54b800d39290e
baligatra <mac@baligatra.com>
2021-02-19 10:56:46 +0800

    init

---
+++ src/main/java/com/example/restspringboot/RestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.restspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootApplication.class, args);
+	}
+
+}
